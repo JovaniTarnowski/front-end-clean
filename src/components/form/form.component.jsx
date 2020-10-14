493d10fc97032344eba188536f4592167cbdc3ee
jovanitarnowski <jovanitarnowski@gmail.com.com>
2020-10-14 19:09:38 -0300

    added css

--- src/components/form/form.component.jsx
+++ src/components/form/form.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './form.styles.css'
 
 class MyForm extends React.Component {
     constructor() {
@@ -12,23 +13,29 @@ class MyForm extends React.Component {
       
       fetch('http://localhost:8000/book/add', {
         method: 'POST',
-        body: data,
-      });
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+      })
     }
   
     render() {
       return (
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="title">Enter the title</label>
+        <form onSubmit={this.handleSubmit} className='input'>
+          <label htmlFor="title">Enter the title: </label>
           <input id="title" name="title" type="text" />
   
-          <label htmlFor="author">Enter the Author</label>
+          <label htmlFor="author">Enter the Author: </label>
           <input id="author" name="author" type="text" />
   
-          <label htmlFor="price">Enter the price</label>
+          <label htmlFor="price">Enter the price: </label>
           <input id="price" name="price" type="text" />
+
+          <label htmlFor="quantity">Enter the price: </label>
+          <input id="quantity" name="quantity" type="text" />
   
-          <button>Send data!</button>
+          <button>Add Book</button>
         </form>
       );
     }
